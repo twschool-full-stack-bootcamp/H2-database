0946a0b38b29f7ac3670e60d990e5362f2cbc36b
jiaoqiqi <jiaoqiqi977@163.com>
2019-04-02 15:49:27 +0800

    add controller

--- src/main/java/com/example/H2database/UserController.java
+++ src/main/java/com/example/H2database/UserController.java
@@ -1,8 +1,18 @@
 package com.example.H2database;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
-//  TODO
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/")
+  List<User> userList() {
+    return userRepository.findAll();
+  }
 }
